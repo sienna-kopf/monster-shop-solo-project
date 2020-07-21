6465fa55a7ae26636de831d20e85cd532de23455
Nick Edwin <nickedwin85@gmail.com>
2020-07-21 16:21:42 -0600

    Updated roles to proper number

--- spec/features/users/merchant_user_spec.rb
+++ spec/features/users/merchant_user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "as a merchant level user" do
   describe "it views any page" do
     it "and sees additonal link to merchant dashboard" do
-      user = User.create(name: "Timmy", role: 1)
+      user = User.create(name: "Timmy", role: 2)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
@@ -13,10 +13,10 @@ RSpec.describe "as a merchant level user" do
     end
 
     it "does not see link to merchant dashboard when logged in as default user" do
-      user = User.create(name: "Bob", role: 0)
+      user = User.create(name: "Bob", role: 1)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      
+
       visit "/"
 
       expect(page).to_not have_link("Dashboard")
