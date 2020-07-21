a49957855106fe18eaae6a21452be304bcb1b121
Nick Edwin <nickedwin85@gmail.com>
2020-07-21 16:22:05 -0600

    Changed enum roles to match user levels

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, require: true
-  has_secure_password 
+  has_secure_password
+
+  enum role: %w(visitor default merchant admin)
 end
