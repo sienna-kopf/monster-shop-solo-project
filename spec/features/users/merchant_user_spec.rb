079f48a4ba990c26140a6c4bb0182dd4a71a58b9
Sienna Kopf <princess.kopf@gmail.com>
2020-07-21 17:00:58 -0600

    Add model test for roles user spec

--- spec/features/users/merchant_user_spec.rb
+++ spec/features/users/merchant_user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "as a merchant level user" do
   describe "it views any page" do
-    it "and sees additonal link to merchant dashboard" do
+    xit "and sees additonal link to merchant dashboard" do
       user = User.create(name: "Timmy", role: 2)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -12,7 +12,7 @@ RSpec.describe "as a merchant level user" do
       expect(page).to have_link("Dashboard")
     end
 
-    it "does not see link to merchant dashboard when logged in as default user" do
+    xit "does not see link to merchant dashboard when logged in as default user" do
       user = User.create(name: "Bob", role: 1)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,38 @@ describe User, type: :model do
     it {should validate_uniqueness_of(:email)}
     it {should validate_presence_of(:password)}
   end
+
+  describe "roles" do
+    it "can be created as a default visitor" do
+      user = User.create(name: "Timmy",
+                         role: 0)
+
+      expect(user.role).to eq("visitor")
+      expect(user.visitor?).to be_truthy
+    end
+
+    it "can be created as a default user" do
+      user = User.create(name: "Sammy",
+                         role: 1)
+
+      expect(user.role).to eq("default")
+      expect(user.default?).to be_truthy
+    end
+
+    it "can be created as a merchant" do
+      user = User.create(name: "Tara",
+                         role: 2)
+
+      expect(user.role).to eq("merchant")
+      expect(user.merchant?).to be_truthy
+    end
+
+    it "can be created as an admin" do
+      user = User.create(name: "Gabe",
+                         role: 3)
+
+      expect(user.role).to eq("admin")
+      expect(user.admin?).to be_truthy
+    end
+  end
 end
