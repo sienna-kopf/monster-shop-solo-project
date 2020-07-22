9fd27a8baea0a71c884882a6d8adfe9bf4286add
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-07-21 20:48:23 -0600

    Added require_user private method and call this method prior to loading the show page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
+  
+  before_action :require_user
+
   def new
+    
   end
 
   def create
@@ -16,6 +20,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    require_user
     @user = User.find(session[:user_id])
   end
 
@@ -23,5 +28,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.permit(:name, :address, :city, :state, :zip, :email, :password)
+  end
+  
+  def require_user
+    render file: "/public/404" unless current_merchant?
   end
 end
