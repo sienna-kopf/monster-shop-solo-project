6f49ddc5c1a4b638c6f0cf7d8cafe0130cdae5d6
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-07-21 20:45:08 -0600

    Merchant::DashboardController now inherits from Merchant::BaseController

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,10 @@
 class Admin::UsersController < ApplicationController
+  before_action :require_admin
+  
   def index
+  end
+  
+  def require_admin
+    render file: "/public/404" unless current_admin?
   end
 end
