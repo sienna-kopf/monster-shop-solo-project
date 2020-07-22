fdaac201626a9f81f423c6c5092879232caff67c
Sienna Kopf <princess.kopf@gmail.com>
2020-07-22 14:38:59 -0600

    Add to create method in sessions controller to deal with authentication. also add conditioinal logic for visitor login attempts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,34 @@
 class SessionsController < ApplicationController
-  before_action :require_user
+  # before_action :require_user, only: [:create]
 
   def new
   end
 
   def create
-    session[:user_id] = current_user.id
-    flash[:success] = "You are now successfully logged in as #{current_user.name}"
-    if current_admin?
-      redirect_to '/admin/dashboard'
-    elsif current_merchant?
-      redirect_to "/merchant/dashboard"
+    if current_user
+      if current_user.authenticate(params[:password])
+        session[:user_id] = current_user.id
+        flash[:success] = "You are now successfully logged in as #{current_user.name}"
+        if current_admin?
+          redirect_to '/admin/dashboard'
+        elsif current_merchant?
+          redirect_to "/merchant/dashboard"
+        else
+          redirect_to "/profile"
+        end
+      else
+        flash[:error] = "Bad Credentials. Try Again!"
+        render :new
+      end
     else
-      redirect_to "/profile"
+      flash[:error] = "You must have an account to log in!"
+      render :new
     end
   end
 
-  private
-
-  def require_user
-    render file: "/public/404" unless current_default?
-  end
+  # private
+  #
+  # def require_user
+  #   render :new unless current_default?
+  # end
 end
