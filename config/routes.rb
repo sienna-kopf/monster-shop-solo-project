79cb888c745c9c3aecf66df7b1d810ef0c185e25
Sienna Kopf <princess.kopf@gmail.com>
2020-07-26 16:02:36 -0600

    Add route for updating an order and change the route name to cancel for an order

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,8 @@ Rails.application.routes.draw do
   get "/orders/new", to: "orders#new"
   post "/orders", to: "orders#create"
   get "/orders/:id", to: "orders#show"
-  get "/orders/:id/cancel", to: "orders#update"
+  get "/orders/:id/cancel", to: "orders#cancel"
+  patch "/orders/:order_id/update", to: "orders#update"
 
   get "/register/new", to: "users#new"
   post "/register", to: "users#create"
