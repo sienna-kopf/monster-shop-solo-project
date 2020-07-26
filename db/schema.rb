20966af42cae8d4341f50612c69137de580e3384
Nick Edwin <nickedwin85@gmail.com>
2020-07-26 17:29:48 -0600

    New foreign key for merchant added to user to check if user is an employee

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200725223452) do
+ActiveRecord::Schema.define(version: 20200726221009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,8 @@ ActiveRecord::Schema.define(version: 20200725223452) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 0
+    t.bigint "merchant_id", default: nil
+    t.index ["merchant_id"], name: "index_users_on_merchant_id"
   end
 
   add_foreign_key "item_orders", "items"
@@ -90,4 +92,5 @@ ActiveRecord::Schema.define(version: 20200725223452) do
   add_foreign_key "items", "merchants"
   add_foreign_key "orders", "users"
   add_foreign_key "reviews", "items"
+  add_foreign_key "users", "merchants"
 end
