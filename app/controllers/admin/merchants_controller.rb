7347938bfd9318066f50898f171050a685eec130
Sienna Kopf <princess.kopf@gmail.com>
2020-07-27 17:09:56 -0600

    Add conditional logic to update action in admin merchants controller

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -9,10 +9,17 @@ class Admin::MerchantsController < Admin::BaseController
 
   def update
     merchant = Merchant.find(params[:id])
-    merchant.toggle(:enabled?)
-    merchant.items.update(enabled?: false)
-    if merchant.save
-      flash[:success] = "#{merchant.name} and all merchant items have been disabled"
+    if merchant.enabled?
+      merchant.toggle(:enabled?)
+      merchant.items.update(enabled?: false)
+      if merchant.save
+        flash[:success] = "#{merchant.name} and all merchant items have been disabled"
+      end
+    else
+      merchant.toggle(:enabled?)
+      if merchant.save
+        flash[:success] = "#{merchant.name} has been enabled"
+      end
     end
     redirect_to "/admin/merchants"
   end
