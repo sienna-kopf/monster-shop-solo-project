1a91d37f453cc18c4c77ff0d080669e452f11aef
Nick Edwin <nickedwin85@gmail.com>
2020-07-28 18:34:10 -0600

    Logic for fulfilling order

--- app/controllers/merchant/orders_controller.rb
+++ app/controllers/merchant/orders_controller.rb
@@ -1,5 +1,17 @@
 class Merchant::OrdersController < Merchant::BaseController
+
   def show
     @order = Order.find(params[:order_id])
+  end
+
+  def update
+    item = Item.find(params[:item_id])
+    order_item = ItemOrder.where('item_orders.item_id = ?', params[:item_id]).first
+    order_item.update(status: "fulfilled")
+    item.decrement!("inventory", by = order_item.quantity)
+    if item.save
+      redirect_back(fallback_location: root_path)
+      flash[:success] = "Item is now fulfilled"
+    end
   end
 end
