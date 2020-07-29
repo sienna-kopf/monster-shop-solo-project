2f296f4a2df804fd306d0e7ae72e77d3e5d74b3f
Sienna Kopf <princess.kopf@gmail.com>
2020-07-29 13:44:36 -0600

    add new find by AR logic to the merchant order items controller update method

---
+++ app/controllers/merchant/order_items_controller.rb
@@ -0,0 +1,14 @@
+class Merchant::OrderItemsController < Merchant::BaseController
+  def update
+    order = Order.find(params[:order_id])
+    item = Item.find(params[:item_id])
+    item_order = ItemOrder.find_by(item_id: params[:item_id], order_id: params[:order_id])
+    item_order.update(status: "fulfilled")
+    item.decrement!("inventory", by = item_order.quantity)
+    if item.save
+      item.save
+      redirect_to "/merchant/orders/#{order.id}"
+      flash[:success] = "Item is now fulfilled"
+    end
+  end
+end
