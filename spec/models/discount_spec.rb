5f97bdb201682c11964d3b9694b486f1278fe25c
Sienna Kopf <princess.kopf@gmail.com>
2020-08-04 12:16:21 -0600

    Add model method test for best discount percentage

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -8,4 +8,17 @@ describe Discount, type: :model do
   describe 'relationships' do
     it {should belong_to :merchant}
   end
+
+  describe 'methods' do
+    before :each do
+      @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+
+      @off_2 = Discount.create(percentage_discount: 10, item_quantity: 2, merchant_id: @megan.id)
+      @off_20_for_2 = Discount.create(percentage_discount: 20, item_quantity: 2, merchant_id: @megan.id)
+    end
+    
+    it '.best_discount' do
+      expect(Discount.best_discount).to eq(20)
+    end
+  end
 end
