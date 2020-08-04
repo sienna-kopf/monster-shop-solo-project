c693f1b34ced0c53b9367bb50fc59536bc192391
Sienna Kopf <princess.kopf@gmail.com>
2020-08-04 10:33:46 -0600

    Add test for orders new page display of discounted items price, subtotal and total

--- spec/features/discounts/order_spec.rb
+++ spec/features/discounts/order_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "when a user goes to checkout" do
-  describe "if any of the items in their cart qualify for a discount" do
+RSpec.describe "when a user clicks on checkout" do
+  describe "They are taken to the orders new page and any discounts are shown" do
     before :each do
       @merchant1 = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       @off_10 = Discount.create(percentage_discount: 10, item_quantity: 10, merchant_id: @merchant1.id)
@@ -14,6 +14,7 @@ RSpec.describe "when a user goes to checkout" do
       @paper = @merchant1.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 30)
       @pencil = @merchant1.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
     end
+
     it "automatically applies that discount" do
       visit "/items/#{@paper.id}"
       click_on "Add To Cart"
@@ -38,6 +39,8 @@ RSpec.describe "when a user goes to checkout" do
         click_on "Increase Amount"
         click_on "Increase Amount"
         click_on "Increase Amount"
+        click_on "Increase Amount"
+        click_on "Increase Amount"
       end
 
       within("#cart-item-#{@pencil.id}") do
@@ -48,74 +51,44 @@ RSpec.describe "when a user goes to checkout" do
         click_on "Increase Amount"
       end
 
-      visit "/cart"
+       click_on "Checkout"
 
-      within "#cart-item-#{@paper.id}" do
-        expect(page).to have_link(@paper.name)
-        expect(page).to have_css("img[src*='#{@paper.image}']")
-        expect(page).to have_link("#{@paper.merchant.name}")
-        expect(page).to have_content("$20.00")
-        expect(page).to have_content("8")
-        expect(page).to have_content("$160.00")
-      end
+       expect(current_path).to eq("/orders/new")
 
-      within "#cart-item-#{@tire.id}" do
-        expect(page).to have_link(@tire.name)
-        expect(page).to have_css("img[src*='#{@tire.image}']")
-        expect(page).to have_link("#{@tire.merchant.name}")
-        expect(page).to have_content("$100.00")
-        expect(page).to have_content("4")
-        expect(page).to have_content("$400.00")
-      end
+       within "#order-item-#{@tire.id}" do
+         expect(page).to have_link(@tire.name)
+         expect(page).to have_link("#{@tire.merchant.name}")
+         expect(page).to have_content("$100.00")
+         expect(page).to have_content("4")
+         expect(page).to have_content("$400.00")
+       end
 
-      within "#cart-item-#{@pencil.id}" do
-        expect(page).to have_link(@pencil.name)
-        expect(page).to have_css("img[src*='#{@pencil.image}']")
-        expect(page).to have_link("#{@pencil.merchant.name}")
-        expect(page).to have_content("$2.00")
-        expect(page).to have_content("6")
-        expect(page).to have_content("$12.00")
-      end
+       within "#order-item-#{@paper.id}" do
+         expect(page).to have_link(@paper.name)
+         expect(page).to have_link("#{@paper.merchant.name}")
+         expect(page).to have_content("$18.00")
+         expect(page).to have_content("Item price has been adjusted due to discount!")
+         expect(page).to have_content("10")
+         expect(page).to have_content("$180.00")
+         expect(page).to have_content("Discount of 10% off 10 items has been applied to #{@paper.name}")
+       end
 
-      expect(page).to have_content("Total: $572.00")
+       within "#order-item-#{@pencil.id}" do
+         expect(page).to have_link(@pencil.name)
+         expect(page).to have_link("#{@pencil.merchant.name}")
+         expect(page).to have_content("$2.00")
+         expect(page).to have_content("6")
+         expect(page).to have_content("$12.00")
+       end
 
-      visit '/cart'
+       expect(page).to have_content("Total: $592")
 
-      within("#cart-item-#{@paper.id}") do
-        click_on "Increase Amount"
-        click_on "Increase Amount"
-      end
-
-      within "#cart-item-#{@tire.id}" do
-        expect(page).to have_link(@tire.name)
-        expect(page).to have_css("img[src*='#{@tire.image}']")
-        expect(page).to have_link("#{@tire.merchant.name}")
-        expect(page).to have_content("$100.00")
-        expect(page).to have_content("4")
-        expect(page).to have_content("$400.00")
-      end
-
-      within "#cart-item-#{@pencil.id}" do
-        expect(page).to have_link(@pencil.name)
-        expect(page).to have_css("img[src*='#{@pencil.image}']")
-        expect(page).to have_link("#{@pencil.merchant.name}")
-        expect(page).to have_content("$2.00")
-        expect(page).to have_content("6")
-        expect(page).to have_content("$12.00")
-      end
-
-      within "#cart-item-#{@paper.id}" do
-        expect(page).to have_link(@paper.name)
-        expect(page).to have_css("img[src*='#{@paper.image}']")
-        expect(page).to have_link("#{@paper.merchant.name}")
-        expect(page).to have_content("$18.00")
-        expect(page).to have_content("Item price has been adjusted due to discount!")
-        expect(page).to have_content("10")
-        expect(page).to have_content("$180.00")
-        expect(page).to have_content("Discount of 10% off 10 items has been applied to #{@paper.name}")
-      end
-
-      expect(page).to have_content("Total: $592.00")
+       expect(page).to have_field(:name)
+       expect(page).to have_field(:address)
+       expect(page).to have_field(:city)
+       expect(page).to have_field(:state)
+       expect(page).to have_field(:zip)
+       expect(page).to have_button("Create Order")
     end
   end
 end
